e362b9c53a2dabc2e726fbd46ef1ca9079a71d0d
Ayushi Rai <raikartik186@gmail.com>
2026-07-29 22:30:51 +0530

    3

--- src/main/java/reports/ExtentManager.java
+++ src/main/java/reports/ExtentManager.java
@@ -1,5 +1,9 @@
 package reports;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
@@ -8,19 +12,30 @@ public class ExtentManager {
 	private static ExtentReports extent;
 	
 	public static ExtentReports getReport() {
-		   if(extent==null){
+			   
+			   if (extent == null) {
 
-	            ExtentSparkReporter spark =
-	                    new ExtentSparkReporter("reports/APIAutomationReport.html");
+		            // Generate folder name using current date and time
+		            String folderName = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss")
+		                    .format(new Date());
 
-	            extent = new ExtentReports();
-	            extent.attachReporter(spark);
+		            // Report path
+		            String reportPath = "reports/" + folderName + "/APIAutomationReport.html";
 
-	            extent.setSystemInfo("Tester","Ayushi Rai");
-	            extent.setSystemInfo("Framework","REST Assured");
-	            extent.setSystemInfo("Environment","QA");
-	        }
+		            // Create folder
+		            new File("reports/" + folderName).mkdirs();
+
+		            // Create HTML report
+		            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+
+		            extent = new ExtentReports();
+		            extent.attachReporter(spark);
+
+		            extent.setSystemInfo("Tester", "Ayushi Rai");
+		            extent.setSystemInfo("Framework", "REST Assured");
+		            extent.setSystemInfo("Environment", "QA");
+		        }
+			   
 		   return extent;
-	}
+	}}
 	
-}
